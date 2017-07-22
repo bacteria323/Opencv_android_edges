bff67ab358b2f8e8d19f7fb4fa7283682c6fa7ae
Shun Lee <shunlee94@hotmail.com>
2017-07-22 16:13:11 +0800

    changed opencv from 2.4.11 to 2.4.13.2

--- app/src/main/java/com/hollow_pebble/liveedges/MainActivity.java
+++ app/src/main/java/com/hollow_pebble/liveedges/MainActivity.java
@@ -3,6 +3,7 @@ package com.hollow_pebble.liveedges;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.WindowManager;
 
 import org.opencv.android.BaseLoaderCallback;
@@ -11,7 +12,7 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+public class MainActivity extends AppCompatActivity  {
 
     //A Tag to filter the log messages
     private static final String TAG = "MainActivity";
@@ -21,28 +22,35 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     //This is the callback object used when we initialize the OpenCV
     //library asynchronously
-    private BaseLoaderCallback mLoaderCallback = new
-            BaseLoaderCallback(this) {
-                @Override
-                //This is the callback method called once the OpenCV //manager is connected
-                public void onManagerConnected(int status) {
-                    switch (status) {
-                        //Once the OpenCV manager is successfully connected we can enable the camera interaction with the defined OpenCV camera view
-                        case LoaderCallbackInterface.SUCCESS:
-                        {
-                            Log.i(TAG, "OpenCV loaded successfully");
-                            mOpenCvCameraView.enableView();
-                        } break;
-                        default:
-                        {
-                            super.onManagerConnected(status);
-                        } break; }
-                } };
+//    private BaseLoaderCallback mLoaderCallback = new
+//            BaseLoaderCallback(this) {
+//                @Override
+//                //This is the callback method called once the OpenCV //manager is connected
+//                public void onManagerConnected(int status) {
+//                    switch (status) {
+//                        //Once the OpenCV manager is successfully connected we can enable the camera interaction with the defined OpenCV camera view
+//                        case LoaderCallbackInterface.SUCCESS:
+//                        {
+//                            Log.i(TAG, "OpenCV loaded successfully");
+//                            //mOpenCvCameraView.enableView();
+//                        } break;
+//                        default:
+//                        {
+//                            super.onManagerConnected(status);
+//                        } break; }
+//                } };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
+
+//        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloVisionView);
+//
+//        //Set the view as visible
+//        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+//        //Register your activity as the callback object to handle //camera frames
+//        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
     @Override
@@ -52,21 +60,24 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         //Call the async initialization and pass the callback object we
         //created later, and chose which version of OpenCV library to //load.
 
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_11, this, mLoaderCallback);
+        //OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_11, this, mLoaderCallback);
     }
 
-    @Override
-    public void onCameraViewStarted(int width, int height) {
-
-    }
-
-    @Override
-    public void onCameraViewStopped() {
-
-    }
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return null;
-    }
+//    @Override
+//    public void onCameraViewStarted(int width, int height) {
+//
+//    }
+//
+//    @Override
+//    public void onCameraViewStopped() {
+//
+//    }
+//
+//    @Override
+//    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+//        //We're returning the colored frame as is to be rendered on
+//        //thescreen.
+//        //return inputFrame.rgba();
+//        return null;
+//    }
 }

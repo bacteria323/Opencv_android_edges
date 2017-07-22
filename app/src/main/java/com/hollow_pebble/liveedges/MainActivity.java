037a1733a7ea33e728f206dc519cdc873e140133
Shun Lee <shunlee94@hotmail.com>
2017-07-22 17:42:14 +0800

    enable live camera preview

--- app/src/main/java/com/hollow_pebble/liveedges/MainActivity.java
+++ app/src/main/java/com/hollow_pebble/liveedges/MainActivity.java
@@ -1,10 +1,16 @@
 package com.hollow_pebble.liveedges;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
